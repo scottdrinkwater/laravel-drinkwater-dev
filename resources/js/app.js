d326e4b153ba747f6ade220d1921e45333de59c5
Scott Drinkwater <s.p.drinkwater@gmail.com>
2024-03-02 21:37:38 +0000

    Install Vue (TS)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
+import { createApp } from "vue";
+import App from "./components/organisms/App.vue";
 import "./bootstrap";
+import "../css/app.css";
+
+const app = createApp(App);
+
+app.mount("#app");
