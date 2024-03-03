46516213677c2615cc76e43645a50e74a1724345
Scott Drinkwater <s.p.drinkwater@gmail.com>
2024-03-03 13:11:51 +0000

    Configure vitest

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    test: {
+        globals: true,
+    },
     plugins: [
         laravel({
             input: ["resources/js/app.js"],
