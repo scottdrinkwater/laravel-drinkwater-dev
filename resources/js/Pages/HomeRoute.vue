8cebd4e13f4b93967ba95fd955b731628166bacb
Scott Drinkwater <s.p.drinkwater@gmail.com>
2024-03-03 13:13:57 +0000

    Add hero banner

--- resources/js/Pages/HomeRoute.vue
+++ resources/js/Pages/HomeRoute.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="content mx-auto lg:text-9xl text-5xl text-center">
-        <h1>SCOTT&nbsp;<strong>DRINKWATER</strong></h1>
+    <div class="content mx-auto">
+        <HeroBanner>
+            <template #heading>SCOTT&nbsp;<strong>DRINKWATER</strong></template>
+            <template #subheading>SOFTWARE ENGINEER</template>
+        </HeroBanner>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import HeroBanner from "../components/HeroBanner/HeroBanner.vue";
+import HighlightGrid from "../components/Grids/HighlightGrid/HighlightGrid.vue";
+</script>
---
+++ resources/js/components/HeroBanner/HeroBanner.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="content mx-auto">
+        <div class="border-b border-b-zinc-900 pb-5 pt-5">
+            <h1 class="lg:text-9xl text-5xl text-center">
+                <slot name="heading" />
+            </h1>
+            <h2 class="lg:text-6xl text-3xl text-center p-3">
+                <slot name="subheading" />
+            </h2>
+        </div>
+    </div>
+</template>
