6914bd58518c5c3fcf85cc8653c5c1ac4128bc5a
Scott Drinkwater <s.p.drinkwater@gmail.com>
2024-03-03 13:14:32 +0000

    Adapt tailwind for Laravel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
     theme: {
         extend: {},
     },
     plugins: [],
-    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 };
